6dfe0e4084f78ff05a3e152d218624861ac1786d
Jack Dempsey <jack.dempsey@gmail.com>
2011-05-25 10:44:35 -0700

    get basic test workign

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -5,7 +5,7 @@ describe "Users" do
     it "works! (now write some real specs)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       get users_path
-      response.status.should be(200)
+      response.body.should include("Listing users")
     end
   end
 end
