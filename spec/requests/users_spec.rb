4a02d0b36124bb227ab9f2c8a0b677ba5e2f01f1
Jack Dempsey <jack.dempsey@gmail.com>
2011-05-26 11:44:55 -0700

    use visit not get. duh

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -4,13 +4,12 @@ describe "Users" do
   describe "GET /users" do
     it "works! (now write some real specs)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get users_path
-      response.body.should include("New User")
+      visit users_path
+      puts body
 
       click_link 'New User'
 
-      response.body.should include("form")
-      response.body.should include("Back")
+      puts body
     end
   end
 end
